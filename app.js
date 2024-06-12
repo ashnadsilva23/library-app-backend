d586f5ee0c8300eb206851b8d4176f92b96d8715
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-06-13 04:19:00 +0530

    project completed

--- app.js
+++ app.js
@@ -1,33 +1,72 @@
 const express =  require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-
+const  {librarymodal} =require("./models/addBook")
 const app = express()
 app.use(cors())
+app.use(express.json())
+mongoose.connect("mongodb+srv://ashna:ashna@cluster0.n9qo4.mongodb.net/libraryDB?retryWrites=true&w=majority&appName=Cluster0")
 
-const addbooks = require("./models/addBook")
-app.get("/",(req,res)=>{
 
-    res.send("hello")
+
+
+app.post("/",(req,res)=>{
+    let input =req.body
+    let library =new librarymodal(input)
+    library.save()
+
+    res.json({"status":"success"})
 })
 
 app.post("/search",(req,res)=>{
-    res.send("welcome")
+    let input = req.body
+    librarymodal.find(input).then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    ).finally()
 
 })
-
-
 
 app.post("/delete",(req,res)=>{
-    res.send("delete")
+    let input=req.body
+    librarymodal.findByIdAndDelete(input._id).then(
+        (response)=>{
+            res.json({"status":"success"})
+        }
+    ).catch(
+        (error)=>{
+            res.json({"status":"success"})
+        }
+    ).finally()
+    
+
 })
+
+
+
+
 
 
 
 app.post("/view",(req,res)=>{
-    res.send("view")
+    librarymodal.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    ).finally()
 })
 
-app.listen(8081,()=>{
+
+
+app.listen(8080,()=>{
     console.log("server started")
 })
